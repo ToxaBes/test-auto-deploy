02f6a1c7e9beb3302680e33c89af7d220a859149
Toxa Bes <toxabes@gmail.com>
2013-09-06 16:19:43 +0300

    test changes

--- deploy.php
+++ deploy.php
@@ -1,11 +1,11 @@
 <?php
 try{
-  $payload = json_decode($_REQUEST['payload']);
-  if ($payload->ref === 'refs/heads/master')
-  {
+  //$payload = json_decode($_REQUEST['payload']);
+  //if ($payload->ref === 'refs/heads/master')
+  //{
   	 $script = dirname(__FILE__) . '/deploy.sh';
      exec($script);
-  }
+  //}
 }catch(Exception $e){
   exit(0);
 }
