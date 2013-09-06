1a19820c34d110ce34b34463684b88cdf7c8dd00
Toxa Bes <toxabes@gmail.com>
2013-09-06 17:04:41 +0300

    test changes 6

--- deploy.php
+++ deploy.php
@@ -1,14 +1,17 @@
 <?php
+$output = shell_exec('git help');
+echo "<pre>$output</pre>";
+
 try{
   //$payload = json_decode($_REQUEST['payload']);
   //if ($payload->ref === 'refs/heads/master')
   //{
-  	 $script = dirname(__FILE__) . '/deploy.sh';
-  	 mail('toxabes@gmail.com', 'Test', $script);
-  	 echo $script;
-     exec($script);
+  	 //$script = dirname(__FILE__) . '/deploy.sh';
+  	 //mail('toxabes@gmail.com', 'Test', $script);
+  	 //echo $script;
+     //exec($script);
   //}
 }catch(Exception $e){
-  exit(0);
+    // echo $e->getMessage();
 }
 ?>
