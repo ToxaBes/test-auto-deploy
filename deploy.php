0c6dddd470fe9ef525a97219c30f007d95caf8ed
Toxa Bes <toxabes@gmail.com>
2013-09-07 14:31:50 +0300

    test 124

--- deploy.php
+++ deploy.php
@@ -1,29 +1,4 @@
 <?php
-//$output = shell_exec('git pull origin master');
-//echo "<pre>$output</pre>";
-
-try{
-  $commands = array(
-		'echo $PWD',
-		'whoami',
-		'git pull',
-		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-	);
- 
-	// Run the commands for output
-	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
-	}
-	echo $output;
-}catch(Exception $e){
-    echo $e->getMessage();
-}
+$output = shell_exec('git pull origin master');
+echo "<pre>$output</pre>";
 ?>
